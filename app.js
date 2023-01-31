b6a9591a012d836d7afecb7f820b10e070321dcf
Rafael Candido Roque de Lima <65678149+rafaellimasnp@users.noreply.github.com>
2023-01-30 23:02:32 -0400

    exclui linhas em branco no arquivo

--- app.js
+++ app.js
@@ -3,4 +3,3 @@ console.log("aplicacao esta se conectando com o banco de dados")
 console.log("Este é um teste de git 1 ")
 console.log("Este é um teste de git 2 ")
 console.log("Este é um teste de git 3 ")
-
