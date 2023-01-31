8b3dfd2d49c041935f0b5dbf78b2cc26aca9e1ac
Rafael Candido Roque de Lima <65678149+rafaellimasnp@users.noreply.github.com>
2023-01-30 23:02:06 -0400

    adicionando mais linhas de testes para git

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicacao esta se conectando com o banco de dados")
+console.log("Este é um teste de git 1 ")
+console.log("Este é um teste de git 2 ")
+console.log("Este é um teste de git 3 ")
+
