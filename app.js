c58598579c824677b66a392bfa7703b67cba75bc
rafaellimasnp <rafaelrock722@gmail.com>
2023-01-30 23:13:43 -0400

    voltei para a versão 438c7bb

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log("aplicacao esta se conectando com o banco de dados")
 console.log("Este é um teste de git 1 ")
 console.log("Este é um teste de git 2 ")
 console.log("Este é um teste de git 3 ")
+
+console.log("Enviando para o git no terminal ")
